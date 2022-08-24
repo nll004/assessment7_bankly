d26156eb9547c87103a349906d35817b75bd8bbe
nll004 <loninath17@gmail.com>
2022-08-23 20:07:34 -0400

    final edits to timeWord.js

--- timeWord.js
+++ timeWord.js
@@ -12,12 +12,12 @@ const minutes2 = ['clock', 'One', 'Two', 'Three', 'Four', 'Five', 'Six',
 /** Accepts a string time(military time) and
  * converts into a string of words that represent the time
  *
- * Ex: 01:32 => One thirty two AM
+ * Ex: "01:32" => "One thirty two AM"
 */
 function convertTimeIntoWords(timeString){
     const time = timeString.split(':');
     let hr = parseInt(time[0]);
-    let min = time[1]; // string value
+    let min1 = time[1]; // string value
     let timePeriod = 'AM';
 
     if (hr >= 12) {
@@ -25,16 +25,16 @@ function convertTimeIntoWords(timeString){
         hr = hr - 12;
     };
 
-    if (min[0] === '1'){
-        min = parseInt(min);
-        console.log(`${hours[hr]} ${minutes2[min]} ${timePeriod}`)
-        return `${hours[hr]} ${minutes2[min]} ${timePeriod}`
+    if (min1[0] === '1'){
+        // if minutes are in the "teens"
+        min1 = parseInt(min1);
+        return `${hours[hr]} ${minutes2[min1]} ${timePeriod}`
     }
     else {
-        let min2 = parseInt(min[1]);
-        min = parseInt(min[0])
-        console.log(`${hours[hr]} ${minutes1[min]} ${minutes2[min2]} ${timePeriod}`)
-        return `${hours[hr]} ${minutes1[min]} ${minutes2[min2]} ${timePeriod}`
+        // if minutes are not in teens, min digits are split between 2 variables
+        let min2 = parseInt(min1[1]);
+        min1 = parseInt(min1[0]);
+        return `${hours[hr]} ${minutes1[min1]} ${minutes2[min2]} ${timePeriod}`
     }
 }
 
